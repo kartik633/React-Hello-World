0d7a3adbde39b665c44f911e5287e3221f8bd286
Kartik <9415kks@gmail.com>
2021-10-08 14:18:18 +0530

    Now you can enter multiple items

--- src/component/basics/Restaurant.js
+++ src/component/basics/Restaurant.js
@@ -2,12 +2,29 @@ import React, {useState} from 'react'
 import "./style.css"
 import Menu from "./menuapi.js";
 import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList = [...new Set(Menu.map((curElem) =>{
+
+  return curElem.category;
+
+})),
+"All",];
+
+console.log(uniqueList);
 
 const Restaurant = () => {
 
    const [menuData, setmenuData] = useState(Menu);
+   const [menuList,setMenuList] = useState(uniqueList);
 
    const filterItem = (category) => {
+
+      if(category === "All"){
+        setmenuData(Menu);
+        return;
+      }
+
      const updatedList = Menu.filter((curElem) => {
         return curElem.category === category;
      });
@@ -17,39 +34,7 @@ const Restaurant = () => {
 
     return (
         <>
-            <nav className="navbar">
-              <div className="btn-group">
-                <button className="btn-group__item" 
-                onClick={() =>
-                filterItem("breakfast")}>
-                  Breakfast
-                </button>
-
-                <button className="btn-group__item"
-                onClick={() =>
-                  filterItem("lunch")}>
-                  Lunch
-                </button>
-
-                <button className="btn-group__item"
-                onClick={() =>
-                  filterItem("evening")}>
-                  Evening
-                </button>
-
-                <button className="btn-group__item"
-                onClick={() =>
-                  filterItem("dinner")}>
-                  Dinner
-                </button>
-
-                <button className="btn-group__item"
-                onClick={() => setmenuData(Menu)}>
-                  All
-                </button>
-
-              </div>
-            </nav>
+            <Navbar filterItem = {filterItem} menuList = {menuList} />
             < MenuCard menuData = {menuData} />
         </>
     )
